b5f214876243b87bc2cfc9d1a28eecf34823e082
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-08-12 17:20:05 +0300

    Create api.js

---
+++ public/api.js
@@ -0,0 +1,17 @@
+let api = {
+  port: "3000",
+  developer: "Kawethra",
+  author: "Kurnaz, Murt",
+  desing: "Kurnaz",
+  functions: {
+    log: function(input){
+      console.log(input);
+    },
+    alert: function(input){
+      alert(input);
+    },
+    getUrl: function(){
+      console.log(window.location.href);
+    },
+  },
+};

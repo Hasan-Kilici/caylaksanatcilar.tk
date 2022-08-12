8785b759e27650181d400c2c089fc123e06cd532
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-08-12 17:20:21 +0300

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,305 @@
+
+const page = { 
+  Kawethra: function (input) {
+    console.log(`Sitenin Geliştiricisi Kawethra ${input} der`);
+  },
+  FreamWork: "V.1.0.0",
+  CSS: function (query, property, value) {
+    let PROPERTY = property.toLowerCase();
+    if (PROPERTY == "background") {
+      document.querySelector(query).style.background = value;
+    }
+    if (PROPERTY == "background-attachment") {
+      document.querySelector(query).style.backgroundAttachment = value;
+    }
+    if (PROPERTY == "background-color") {
+      document.querySelector(query).style.backgroundColor = value;
+    }
+    if (PROPERTY == "background-image") {
+      document.querySelector(query).style.backgroundImage = value;
+    }
+    if (PROPERTY == "background-position") {
+      document.querySelector(query).style.backgroundPosition = value;
+    }
+    if (PROPERTY == "background-repeat") {
+      document.querySelector(query).style.backgroundRepeat = value;
+    }
+    if (PROPERTY == "border") {
+      document.querySelector(query).style.border = value;
+    }
+    if (PROPERTY == "border-collapse") {
+      document.querySelector(query).style.borderCollapse = value;
+    }
+    if (PROPERTY == "border-color") {
+      document.querySelector(query).style.borderColor = value;
+    }
+    if (PROPERTY == "border-top-color") {
+      document.querySelector(query).style.borderTopColor = value;
+    }
+    if (PROPERTY == "border-right-color") {
+      document.querySelector(query).style.borderRightColor = value;
+    }
+    if (PROPERTY == "border-bottom-color") {
+      document.querySelector(query).style.borderBottomColor = value;
+    }
+    if (PROPERTY == "border-left-color") {
+      document.querySelector(query).style.borderLeftColor = value;
+    }
+    if (PROPERTY == "border-spacing") {
+      document.querySelector(query).style.borderSpacing = value;
+    }
+    if (PROPERTY == "border-style") {
+      document.querySelector(query).style.borderStyle = value;
+    }
+    if (PROPERTY == "border-top-style") {
+      document.querySelector(query).style.borderTopStyle = value;
+    }
+    if (PROPERTY == "border-left-style") {
+      document.querySelector(query).style.borderLeftStyle = value;
+    }
+    if (PROPERTY == "border-bottom-style") {
+      document.querySelector(query).style.borderBottomStyle = value;
+    }
+    if (PROPERTY == "border-right-style") {
+      document.querySelector(query).style.borderRightStyle = value;
+    }
+    if (PROPERTY == "border-top") {
+      document.querySelector(query).style.borderTop = value;
+    }
+    if (PROPERTY == "border-left") {
+      document.querySelector(query).style.borderLeft = value;
+    }
+    if (PROPERTY == "border-bottom") {
+      document.querySelector(query).style.borderBottom = value;
+    }
+    if (PROPERTY == "border-right") {
+      document.querySelector(query).style.borderRight = value;
+    }
+    if (PROPERTY == "border-width") {
+      document.querySelector(query).style.borderWidth = value;
+    }
+    if (PROPERTY == "border-top-width") {
+      document.querySelector(query).style.borderTopWidth = value;
+    }
+    if (PROPERTY == "border-left-width") {
+      document.querySelector(query).style.borderLeftWidth = value;
+    }
+    if (PROPERTY == "border-bottom-width") {
+      document.querySelector(query).style.borderBottomWidth = value;
+    }
+    if (PROPERTY == "border-right-width") {
+      document.querySelector(query).style.borderRightWidth = value;
+    }
+    if (PROPERTY == "bottom") {
+      document.querySelector(query).style.bottom = value;
+    }
+    if (PROPERTY == "caption-side") {
+      document.querySelector(query).style.captionSide = value;
+    }
+    if (PROPERTY == "clear") {
+      document.querySelector(query).style.clear = value;
+    }
+    if (PROPERTY == "color") {
+      document.querySelector(query).style.color = value;
+    }
+    if (PROPERTY == "content") {
+      document.querySelector(query).style.content = value;
+    }
+    if (PROPERTY == "letter-spacing") {
+      document.querySelector(query).style.letterSpacing = value;
+    }
+    if (PROPERTY == "cursor") {
+      document.querySelector(query).style.cursor = value;
+    }
+    if (PROPERTY == "direction") {
+      document.querySelector(query).style.direction = value;
+    }
+    if (PROPERTY == "display") {
+      document.querySelector(query).style.display = value;
+    }
+    if (PROPERTY == "empty-cells") {
+      document.querySelector(query).style.emptyCells = value;
+    }
+    if (PROPERTY == "float") {
+      document.querySelector(query).style.float = value;
+    }
+    if (PROPERTY == "font-family") {
+      document.querySelector(query).style.fontFamily = value;
+    }
+    if (PROPERTY == "font-size") {
+      document.querySelector(query).style.fontSize = value;
+    }
+    if (PROPERTY == "font-style") {
+      document.querySelector(query).style.fontStyle = value;
+    }
+    if (PROPERTY == "font-variant") {
+      document.querySelector(query).style.fontVariant = value;
+    }
+    if (PROPERTY == "font-weight") {
+      document.querySelector(query).style.fontWeight = value;
+    }
+    if (PROPERTY == "font") {
+      document.querySelector(query).style.font = value;
+    }
+    if (PROPERTY == "height") {
+      document.querySelector(query).style.height = value;
+    }
+    if (PROPERTY == "left") {
+      document.querySelector(query).style.left = value;
+    }
+    if (PROPERTY == "line-height") {
+      document.querySelector(query).style.lineHeight = value;
+    }
+    if (PROPERTY == "list-style-image") {
+      document.querySelector(query).style.listStyleImage = value;
+    }
+    if (PROPERTY == "list-style-position") {
+      document.querySelector(query).style.listStylePosition = value;
+    }
+    if (PROPERTY == "list-style-type") {
+      document.querySelector(query).style.listStyleType = value;
+    }
+    if (PROPERTY == "list-style") {
+      document.querySelector(query).style.listStyle = value;
+    }
+    if (PROPERTY == "margin") {
+      document.querySelector(query).style.margin = value;
+    }
+    if (PROPERTY == "margin-top") {
+      document.querySelector(query).style.marginTop = value;
+    }
+    if (PROPERTY == "margin-left") {
+      document.querySelector(query).style.marginLeft = value;
+    }
+    if (PROPERTY == "margin-bottom") {
+      document.querySelector(query).style.marginBottom = value;
+    }
+    if (PROPERTY == "margin-right") {
+      document.querySelector(query).style.marginRight = value;
+    }
+    if (PROPERTY == "max-height") {
+      document.querySelector(query).style.maxHeight = value;
+    }
+    if (PROPERTY == "max-width") {
+      document.querySelector(query).style.maxWidth = value;
+    }
+    if (PROPERTY == "orphans") {
+      document.querySelector(query).style.orphans = value;
+    }
+    if (PROPERTY == "outline-color") {
+      document.querySelector(query).style.outlineColor = value;
+    }
+    if (PROPERTY == "outline-style") {
+      document.querySelector(query).style.outlineStyle = value;
+    }
+    if (PROPERTY == "outline-width") {
+      document.querySelector(query).style.outlineWidth = value;
+    }
+    if (PROPERTY == "outline") {
+      document.querySelector(query).style.outline = value;
+    }
+    if (PROPERTY == "overflow") {
+      document.querySelector(query).style.overflow = value;
+    }
+    if (PROPERTY == "padding") {
+      document.querySelector(query).style.padding = value;
+    }
+    if (PROPERTY == "padding-top") {
+      document.querySelector(query).style.paddingTop = value;
+    }
+    if (PROPERTY == "padding-left") {
+      document.querySelector(query).style.paddingLeft = value;
+    }
+    if (PROPERTY == "padding-bottom") {
+      document.querySelector(query).style.paddingBottom = value;
+    }
+    if (PROPERTY == "padding-right") {
+      document.querySelector(query).style.paddingRight = value;
+    }
+    if (PROPERTY == "position") {
+      document.querySelector(query).style.position = value;
+    }
+    if (PROPERTY == "quotes") {
+      document.querySelector(query).style.quotes = value;
+    }
+    if (PROPERTY == "right") {
+      document.querySelector(query).style.right = value;
+    }
+    if (PROPERTY == "table-layout") {
+      document.querySelector(query).style.tableLayout = value;
+    }
+    if (PROPERTY == "text-align") {
+      document.querySelector(query).style.textAlign = value;
+    }
+    if (PROPERTY == "text-decoration") {
+      document.querySelector(query).style.textDecoration = value;
+    }
+    if (PROPERTY == "text-indent") {
+      document.querySelector(query).style.textIndent = value;
+    }
+    if (PROPERTY == "text-transform") {
+      document.querySelector(query).style.textTransform = value;
+    }
+    if (PROPERTY == "top") {
+      document.querySelector(query).style.top = value;
+    }
+    if (PROPERTY == "vertical-align") {
+      document.querySelector(query).style.verticalAlign = value;
+    }
+    if (PROPERTY == "visibility") {
+      document.querySelector(query).style.visibility = value;
+    }
+    if (PROPERTY == "white-space") {
+      document.querySelector(query).style.whiteSpace = value;
+    }
+    if (PROPERTY == "width") {
+      document.querySelector(query).style.width = value;
+    }
+    if (PROPERTY == "word-spacing") {
+      document.querySelector(query).style.wordSpacing = value;
+    }
+    if (PROPERTY == "z-index") {
+      document.querySelector(query).style.zIndex = value;
+    }
+  },
+  whoIs: "XD ?",
+  link: window.location.href,
+  show: function (query) {
+    document.querySelector(query).style.display = "block";
+  },
+  hide: function (query) {
+    document.querySelector(query).style.display = "none";
+  },
+  controlProtocol: function (s) {
+    for (let i = 0; i < s; i++) {
+      if (i == 10) {
+        console.log("10 Nesne incelendi");
+      }
+      if (i == 100) {
+        console.log("100 Nesne incelendi");
+      }
+      if (i == 200) {
+        console.log("200 Nesne incelendi");
+      }
+      if (i == 300) {
+        console.log("300 Nesne incelendi");
+      }
+      if (i == 400) {
+        console.log("400 Nesne incelendi");
+      }
+      if (i == 500) {
+        console.log("500 Nesne incelendi");
+      }
+      if (i == 999) {
+        console.log("1000 Nesne incelendi!");
+      }
+    }
+  },
+  url: window.location.href,
+};
+
+window.onload = () => {
+  page.controlProtocol(1000);
+  page.Kawethra();
+  console.log(`Client ${page.url} Sayfasını görüntülüyor`)
+};
